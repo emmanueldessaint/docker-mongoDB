8dbbdf0cc4e792924d365c35a78db624ac23420e
emmanuel <emmanueldessaint27220@gmail.com>
2024-02-16 10:42:03 +0100

    update main

--- javascript-app/main.js
+++ javascript-app/main.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose');
+
 console.log('step 1')
-const dbURI = 'mongodb://localhost:27017';
+const dbURI = 'mongodb://localhost:27017/pizzas_orders_db';
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
+const pizzaOrderSchema = new mongoose.Schema({
+  name: String,
+  size: String,
+  price: Number,
+  quantity: Number,
+  date: Date,
+});
+
+const PizzaOrder = mongoose.model('PizzaOrder', pizzaOrderSchema);
+
 mongoose.connect(dbURI, options)
-  .then(() => {
-    console.log('connexion ok');
+  .then(async () => {
+    console.log('Connexion à la base de données réussie');
+
+    const orders = await PizzaOrder.find();
+
+    console.log('Contenu de la base de données :', orders);
   })
-  .catch((err) => console.error('err de connexion', err));
+  .catch((err) => console.error('Erreur de connexion', err));
